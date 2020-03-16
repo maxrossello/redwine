db0b511f9fc5ae7e33cd90b62167bdb812441d97
Massimo Rossello <massimo.rossello@azcomtech.com>
2020-03-16 22:29:07 +0100

    Redwine 4.1.0

--- init.rb
+++ init.rb
@@ -28,10 +28,10 @@ Redmine::Plugin.register :redwine do
   name 'Redwine Core'
   author 'Massimo Rossello'
   description 'Redwine profile for Redmine. Contains main customizations and fixes wrt Redmine core code'
-  version '4.0.6'
+  version '4.1.0'
   url 'https://github.com/maxrossello/redwine.git'
   author_url 'https://github.com/maxrossello'
-  requires_redmine :version => '4.0.6'
+  requires_redmine :version => '4.1.0'
 end 
 
 require_dependency 'imap_patch'
