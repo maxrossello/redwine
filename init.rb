2f7935472cbb041b28ee3c86789cfedf7a7e95a8
Massimo Rossello <counseling.mr@gmail.com>
2023-12-07 17:39:31 +0100

    Preparing version 5.1.1

--- init.rb
+++ init.rb
@@ -28,10 +28,10 @@ Redmine::Plugin.register :redwine do
   name 'Redwine Core'
   author 'Massimo Rossello'
   description 'Redwine profile for Redmine. Contains main customizations and fixes wrt Redmine core code. Defines consistent set of tested plugin versions.'
-  version '5.1.0'
+  version '5.1.1'
   url 'https://github.com/maxrossello/redwine.git'
   author_url 'https://github.com/maxrossello'
-  requires_redmine :version => '5.1.0'
+  requires_redmine :version => '5.1.1'
 end 
 
 require_relative 'lib/imap_patch'
